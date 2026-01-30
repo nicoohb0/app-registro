49fab59ad27c58468aa56ed4ed7cfc8b8b5d18ec
nicoo956 <nicooh026@gmail.com>
2026-01-30 19:18:45 +0100

    update plantilla
    He modificado nuevos archivos y ficheros

---
+++ routes/registro.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const route = express.Router();
+import controladorRegistro from '../controller/'
+
+route.post('/:id');
+route.get('/:id');
+route.put('/:id');
+route.delete('/:id');
