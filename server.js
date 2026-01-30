c624954052584a95c0e5a8fab8186e7aef5cced0
nicoo956 <nicooh026@gmail.com>
2026-01-30 19:09:29 +0100

    Arranque server nodejs
    Configuración básica para iniciar el puerto de la aplicación

---
+++ server.js
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+try {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT,()=> console.log('Servidor activo en el pureto ' + PORT))
+} catch(e) {
+    console.log(e);
+}
